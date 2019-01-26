c74b24842fc65bf7afb8e2c3e42a3d8e0d4c237c
root <root@vultr.guest>
2019-01-26 04:18:34 +0000

    first commit

---
+++ spiders/manga_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy_splash import SplashRequest
+#store urls of target websites here
+from toCrawls import urls
+
+class mangaSpider(scrapy.Spider):
+    name = "manga"
+
+    def start_requests(self):
+        #urls = ["http://comic.kukudm.com/comiclist/2035/68893/2.htm",]
+        for url in urls:
+            yield SplashRequest(url,self.parse,
+                endpoint='render.html',
+                args={'wait':0.5},
+            )
+
+    def parse(self,response):
+        # part = response.url.split("/")[-1]
+        # page = part.split(".")[-2]
+        # filename = 'manga-%s.html' % page
+        # with open(filename, 'wb' ) as f:
+        #     f.write(response.body)
+        # self.log('Saved file %s' % filename)
+        yield {
+		'img': response.css('a img').extract_first()
+	}
---
+++ spiders/toCrawls.py
@@ -0,0 +1,1 @@
+urls = ['http://comic.kukudm.com/comiclist/2274/68949/1.htm',]
