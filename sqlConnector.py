aae421288530f3be14dc52d3f67b8ebae199c927
Wai Tou Sam <sam201024@gmail.com>
2019-01-30 10:08:33 +0800

    sqlConnector inited

--- sqlConnector.py
+++ sqlConnector.py
@@ -5,19 +5,26 @@ from mysql.connector import Error
 def connect():
     """ Connect to MySQL database """
     try:
-        conn = mysql.connector.connect(host='127.0.0.1:3306',
+        conn = mysql.connector.connect(host='127.0.0.1',
                                        database='mangaList',
                                        user='root',
                                        password='admin')
         if conn.is_connected():
             print('Connected to MySQL database')
+
+        return conn
  
     except Error as e:
         print(e)
- 
-    finally:
-        conn.close()
- 
+
+def getLatest(name):
+    conn = connect()
+    cursor = conn.cursor()
+    queryComp = "SELECT LATEST FROM manga WHERE name = %s"
+    cursor.execute(queryComp,name)
+    temp = cursor.fetchone();
+    if(temp != None):
+        return temp[0]
  
 if __name__ == '__main__':
     connect()
